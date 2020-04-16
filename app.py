5a8b2b8aef7fb5db6617674d463cfc63909d7e81
Catherine Denis <catherine-denis@outlook.com>
2020-04-16 04:33:45 -0400

    /add_book and CSS
    Adding a book to the database. CSS updated a bit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template,abort
+from flask import (Flask, render_template,abort,redirect,url_for,request)
 app = Flask(__name__)
-from model import db
+from model import db, save_db
 @app.route('/')
 def index():
     return render_template(
@@ -22,6 +22,28 @@ def lib_page_view(indx):
         return render_template("library.html", book=book, indx=indx, max_indx= len(db)-1)
     except IndexError:
         abort(404)
+        
+@app.route('/add_book', methods=["GET", "POST"])
+def add_book():
+    if request.method == 'POST':
+        book = {
+            "title": request.form['title'],
+            "author": request.form['author'],
+            "isbn": request.form['isbn'],
+            "publication_year": request.form['publication_year'], 
+            "publisher": request.form['publisher'], 
+            "image_url_s": request.form['image_url_s'], 
+            "image_url_m": request.form['image_url_m'], 
+            "image_url_l": request.form['image_url_l'], 
+            "copies": request.form['copies'], 
+            "available": request.form['available']
+        }
+        db.append(book)
+        save_db()
+        return redirect(url_for('lib_page_view',indx= len(db)-1))
+    else:
+        return render_template("add_book.html")
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -4,6 +4,11 @@ import json
 def load_db():
     with open("cscl_test_db.json") as f:
         return json.load(f)
+
+#save to the database
+def save_db():
+    with open("cscl_test_db.json","w") as f:
+        return json.dump(db,f)
         
 #use this variable to access database
 db = load_db()
