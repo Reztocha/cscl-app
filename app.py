bbf678469c31c04ea47639f13f3a6f58ed999a70
Catherine Denis <catherine-denis@outlook.com>
2020-04-15 20:08:43 -0400

    /cscl
    Route added with test .json file (from mongodb database)
    First title and author displayed

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
-
-@app.route('/index')
+from model import db
+@app.route('/')
 def index():
-    return render_template('index.html')
-
-
+    return render_template(
+        'index.html', 
+        message = "Next time on Yu-Gi-Oh!..."
+    )
+    
+@app.route("/cscl")
+def lib_view():
+    book = db[0]
+    return render_template("library.html", book=book)
 
 if __name__ == '__main__':
     app.run(debug=True)
